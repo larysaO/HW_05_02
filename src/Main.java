a006ccb2e2ca0c46fceff690ef32029853b4cfcb
Larysa <onilarisa@gmail.com>
2025-02-05 12:02:59 +0100

    add Person class

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ boolean isPersonExists(Person[] person) - метод, который есть л
 */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Person person=new Person("Jack", 20);
+        System.out.println(person);
     }
 }
